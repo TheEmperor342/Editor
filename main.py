130d876dbcfd7000f240f708c89ebf384521cc07
TheEmperor342 <83999665TheEmperor342@users.noreply.github.com>
2021-06-17 16:49:12 +0530

    added files

---
+++ main.py
@@ -0,0 +1,182 @@
+from modules.ui_main import Ui_MainWindow
+from modules.imports import *
+from os import system
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # ==> MAKING UI
+        # /////////////////////////////////////////////////
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # /////////////////////////////////////////////////
+
+        # ==> REMOVING TITLE BAR
+        # /////////////////////////////////////////////////
+
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+
+        self.ui.titleBar.mouseMoveEvent = self.moveWindow
+        self.ui.topBar.mouseMoveEvent = self.moveWindow
+
+        # /////////////////////////////////////////////////
+
+        # ==> CONNECTING BUTTONS
+        # /////////////////////////////////////////////////
+
+        self.ui.closeBtn.clicked.connect(self.close)
+        self.ui.maxBtn.clicked.connect(self.maxMin)
+        self.ui.minimizeBtn.clicked.connect(self.showMinimized)
+
+        self.ui.saveFileBtn.clicked.connect(self.saveFile)
+        self.ui.saveAsBtn.clicked.connect(self.saveAs)
+        self.ui.openFileBtn.clicked.connect(self.openFile)
+        self.ui.runFileBtn.clicked.connect(self.runFile)
+
+        # /////////////////////////////////////////////////
+
+        # ==> VARIABLES DEFINITIONS
+        # /////////////////////////////////////////////////
+
+        self.msg = QMessageBox()
+        self.APP_STATE = False
+        self.FILE_DATA = {"Path": None}
+
+        # /////////////////////////////////////////////////
+
+    # ==> RUN FILE                      -> CONNECTED TO 'self.ui.runFileBtn'
+    # /////////////////////////////////////////////////
+
+    def runFile(self) -> None:
+        path = self.FILE_DATA["Path"]
+        if path is not None and path.endswith(".py"): system(f'start python "{path}"')
+        else:
+            self.msg.setWindowTitle("Warning")
+            self.msg.setIcon(QMessageBox.Information)
+            self.msg.setText("Either The File Type is not Python or the file isn't saved.")
+            self.msg.exec_()
+
+    # /////////////////////////////////////////////////
+
+    # ==> OPEN FILE                     -> CONNECTED TO 'self.ui.openFileBtn'
+    # /////////////////////////////////////////////////
+
+    def openFile(self) -> None:
+        saveLocation = QFileDialog.getOpenFileName(
+            self,
+            "Location of the File to be Opened",
+            "/",
+            filter = "Python File (*.py) ;; All Files (*.*)"
+        )
+        with open(saveLocation[0], 'r') as f:
+            self.ui.textEdit.setText(f.read())
+            self.FILE_DATA["Path"] = str(saveLocation[0])
+
+    # /////////////////////////////////////////////////
+
+    # ==> SAVE AS                       -> CONNECTED TO 'self.ui.saveAsBtn'
+    # /////////////////////////////////////////////////
+
+    def saveAs(self) -> None:
+        saveLocation = QFileDialog.getSaveFileName(
+            self,
+            "Location to Save the File",
+            "/",
+            filter = "Python File (*.py) ;; All Files (*.*)"
+        )
+        try:
+            with open(saveLocation[0], 'w') as f:
+                f.write(self.ui.textEdit.toPlainText())
+                self.FILE_DATA["Path"] = str(saveLocation[0])
+
+        except Exception as e:
+            self.msg.setWindowTitle("Error")
+            self.msg.setText(f"An Exception occured whilst trying to save that file:\n\t{e}")
+
+            self.msg.setIcon(QMessageBox.Critical)
+            self.msg.exec_()
+
+    # /////////////////////////////////////////////////
+
+    # ==> TO SAVE THE FILE              -> CONNECTED TO 'self.ui.saveFileBtn'
+    # /////////////////////////////////////////////////
+
+    def saveFile(self) -> None:
+        if self.FILE_DATA["Path"] is None:
+            saveLocation = QFileDialog.getSaveFileName(
+                self,
+                "Location to Save the File",
+                "/",
+                filter = "Python File (*.py) ;; All Files (*.*)"
+            )
+            try:
+                with open(saveLocation[0], 'w') as f:
+                    f.write(self.ui.textEdit.toPlainText())
+                    self.FILE_DATA["Path"] = str(saveLocation[0])
+
+            except Exception as e:
+                self.msg.setWindowTitle("Error")
+                self.msg.setText(f"An Exception occured whilst trying to save that file:\n\t{e}")
+
+                self.msg.setIcon(QMessageBox.Critical)
+                self.msg.exec_()
+
+        else:
+            try:
+                with open(self.FILE_DATA["Path"], 'w') as f:
+                    f.write(self.ui.textEdit.toPlainText())
+
+            except Exception as e:
+                self.msg.setWindowTitle("Error")
+                self.msg.setText(f"An Exception occured whilst trying to save that file:\n\t{e}")
+
+                self.msg.setIcon(QMessageBox.Critical)
+                self.msg.exec_()
+
+    # /////////////////////////////////////////////////
+
+    # ==> TO MAXIMIZE/NORMALIZE WINDOW  -> CONNECTED TO 'self.ui.maxBtn'
+    # /////////////////////////////////////////////////
+
+    def maxMin(self) -> None:
+        if not self.APP_STATE:
+            self.showMaximized()
+            self.APP_STATE = True
+            self.ui.maxBtn.setIcon(
+                QIcon(u":/Icons/img/icons/cil-window-restore.png")
+            )
+            self.ui.maxBtn.setToolTip("Restore")
+        else:
+            self.showNormal()
+            self.APP_STATE = False
+            self.ui.maxBtn.setIcon(
+                QIcon(u":/Icons/img/icons/cil-window-maximize.png")
+            )
+            self.ui.maxBtn.setToolTip("Maximize")
+
+    # /////////////////////////////////////////////////
+
+    # ==> MOVE WINDOW WHEN TITLE BAR PRESSED
+    # /////////////////////////////////////////////////
+
+    def moveWindow(self, event) -> None:
+        if event.buttons() == Qt.LeftButton and not self.APP_STATE:
+            self.move(self.pos()+event.globalPos()-self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()
+
+    def mousePressEvent(self, event): self.dragPos = event.globalPos()
+
+    # /////////////////////////////////////////////////
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    win = MainWindow()
+    win.show()
+    app.exec_()
---
+++ modules/ui_main.py
@@ -0,0 +1,329 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainDkgXhw.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from modules.imports import *
+
+import Resources_rc
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.bg = QFrame(self.centralwidget)
+        self.bg.setObjectName(u"bg")
+        self.bg.setStyleSheet(u"/* /////////////////////////////////////////\n"
+"BACKGROUND COLOR OF THE APP */\n"
+"#bg{background-color: rgb(40, 44, 52)}\n"
+"\n"
+"/* /////////////////////////////////////////\n"
+"LABEL STYLESHEET */\n"
+"QLabel{\n"
+"	color: rgb(230, 230, 230);\n"
+"	font-family: \"Segoe UI\";\n"
+"	font-size: 15px\n"
+"}\n"
+"/* /////////////////////////////////////////\n"
+"TOOL TIP */\n"
+"QToolTip {\n"
+"	color: #ffffff;\n"
+"	background-color: rgba(33, 37, 43, 180);\n"
+"	border: 1px solid rgb(44, 49, 58);\n"
+"	background-image: none;\n"
+"	background-position: left center;\n"
+"    background-repeat: no-repeat;\n"
+"	border: none;\n"
+"	border-left: 2px solid rgb(255, 121, 198);\n"
+"	text-align: left;\n"
+"	padding-left: 8px;\n"
+"	margin: 0px;\n"
+"}")
+        self.bg.setFrameShape(QFrame.StyledPanel)
+        self.bg.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.bg)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.topBar = QFrame(self.bg)
+        self.topBar.setObjectName(u"topBar")
+        self.topBar.setMaximumSize(QSize(16777215, 50))
+        self.topBar.setSizeIncrement(QSize(0, 50))
+        self.topBar.setStyleSheet(u"QFrame{background-color: rgb(33, 37, 43);}\n"
+"\n"
+"/* /////////////////////////////////////////\n"
+"TOP BAR BUTTONS STYLE SHEET*/\n"
+"QPushButton{\n"
+"	border: none;\n"
+"	background-color: rgb(33, 37, 43);\n"
+"}\n"
+"QPushButton:hover{	\n"
+"	background-color: rgb(40, 44, 52);\n"
+"}\n"
+"QPushButton:pressed{\n"
+"	background-color: rgb(14, 16, 18)\n"
+"}\n"
+"")
+        self.topBar.setFrameShape(QFrame.StyledPanel)
+        self.topBar.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.topBar)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.titleBar = QLabel(self.topBar)
+        self.titleBar.setObjectName(u"titleBar")
+        self.titleBar.setStyleSheet(u"")
+
+        self.horizontalLayout.addWidget(self.titleBar)
+
+        self.runFileBtn = QPushButton(self.topBar)
+        self.runFileBtn.setObjectName(u"runFileBtn")
+        self.runFileBtn.setMinimumSize(QSize(25, 25))
+        self.runFileBtn.setMaximumSize(QSize(25, 25))
+        icon = QIcon()
+        icon.addFile(u":/Icons/img/icons/cil-media-play.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.runFileBtn.setIcon(icon)
+
+        self.horizontalLayout.addWidget(self.runFileBtn)
+
+        self.openFileBtn = QPushButton(self.topBar)
+        self.openFileBtn.setObjectName(u"openFileBtn")
+        self.openFileBtn.setMinimumSize(QSize(25, 25))
+        self.openFileBtn.setMaximumSize(QSize(25, 25))
+        icon1 = QIcon()
+        icon1.addFile(u":/Icons/img/icons/cil-folder-open.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.openFileBtn.setIcon(icon1)
+
+        self.horizontalLayout.addWidget(self.openFileBtn)
+
+        self.saveAsBtn = QPushButton(self.topBar)
+        self.saveAsBtn.setObjectName(u"saveAsBtn")
+        self.saveAsBtn.setMinimumSize(QSize(25, 25))
+        self.saveAsBtn.setMaximumSize(QSize(25, 25))
+        icon2 = QIcon()
+        icon2.addFile(u":/Icons/img/icons/cil-notes.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.saveAsBtn.setIcon(icon2)
+
+        self.horizontalLayout.addWidget(self.saveAsBtn)
+
+        self.saveFileBtn = QPushButton(self.topBar)
+        self.saveFileBtn.setObjectName(u"saveFileBtn")
+        self.saveFileBtn.setMinimumSize(QSize(25, 25))
+        self.saveFileBtn.setMaximumSize(QSize(25, 25))
+        icon3 = QIcon()
+        icon3.addFile(u":/Icons/img/icons/cil-save.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.saveFileBtn.setIcon(icon3)
+
+        self.horizontalLayout.addWidget(self.saveFileBtn)
+
+        self.minimizeBtn = QPushButton(self.topBar)
+        self.minimizeBtn.setObjectName(u"minimizeBtn")
+        self.minimizeBtn.setMinimumSize(QSize(25, 25))
+        self.minimizeBtn.setMaximumSize(QSize(25, 25))
+        icon4 = QIcon()
+        icon4.addFile(u":/Icons/img/icons/cil-window-minimize.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.minimizeBtn.setIcon(icon4)
+
+        self.horizontalLayout.addWidget(self.minimizeBtn)
+
+        self.maxBtn = QPushButton(self.topBar)
+        self.maxBtn.setObjectName(u"maxBtn")
+        self.maxBtn.setMinimumSize(QSize(25, 25))
+        self.maxBtn.setMaximumSize(QSize(25, 25))
+        icon5 = QIcon()
+        icon5.addFile(u":/Icons/img/icons/cil-window-maximize.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.maxBtn.setIcon(icon5)
+
+        self.horizontalLayout.addWidget(self.maxBtn)
+
+        self.closeBtn = QPushButton(self.topBar)
+        self.closeBtn.setObjectName(u"closeBtn")
+        self.closeBtn.setMinimumSize(QSize(25, 25))
+        self.closeBtn.setMaximumSize(QSize(25, 25))
+        self.closeBtn.setStyleSheet(u"#closeBtn:pressed{\n"
+"	background-color: \"#ff5555\"\n"
+"}")
+        icon6 = QIcon()
+        icon6.addFile(u":/Icons/img/icons/cil-x.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.closeBtn.setIcon(icon6)
+
+        self.horizontalLayout.addWidget(self.closeBtn)
+
+
+        self.verticalLayout_2.addWidget(self.topBar)
+
+        self.frame = QFrame(self.bg)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.textEdit = QTextEdit(self.frame)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setStyleSheet(u"QTextEdit{\n"
+"	background-color: rgb(40, 44, 52);\n"
+"	border: 1px solid white;\n"
+"	color: white;\n"
+"}\n"
+"QScrollBar:horizontal {\n"
+"    border: none;\n"
+"    background: rgb(52, 59, 72);\n"
+"    height: 8px;\n"
+"    margin: 0px 21px 0 21px;\n"
+"	border-radius: 0px;\n"
+"}\n"
+"QScrollBar::handle:horizontal {\n"
+"    background: rgb(189, 147, 249);\n"
+"    min-width: 25px;\n"
+"	border-radius: 4px\n"
+"}\n"
+"QScrollBar::add-line:horizontal {\n"
+"    border: none;\n"
+"    background: rgb(55, 63, 77);\n"
+"    width: 20px;\n"
+"	border-top-right-radius: 4px;\n"
+"    border-bottom-right-radius: 4px;\n"
+"    subcontrol-position: right;\n"
+"    subcontrol-origin: margin;\n"
+"}\n"
+"QScrollBar::sub-line:horizontal {\n"
+"    border: none;\n"
+"    background: rgb(55, 63, 77);\n"
+"    width: 20px;\n"
+"	border-top-left-radius: 4px;\n"
+"    border-bottom-left-radius: 4px;\n"
+"    subcontrol-position: left;\n"
+"    subcontrol-origin: margin;\n"
+"}\n"
+"QScrollBar::up-arrow:horizontal, QScrollBar::down-arrow:horizontal\n"
+"{\n"
+""
+                        "     background: none;\n"
+"}\n"
+"QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal\n"
+"{\n"
+"     background: none;\n"
+"}\n"
+" QScrollBar:vertical {\n"
+"	border: none;\n"
+"    background: rgb(52, 59, 72);\n"
+"    width: 8px;\n"
+"    margin: 21px 0 21px 0;\n"
+"	border-radius: 0px;\n"
+" }\n"
+" QScrollBar::handle:vertical {	\n"
+"	background: rgb(189, 147, 249);\n"
+"    min-height: 25px;\n"
+"	border-radius: 4px\n"
+" }\n"
+" QScrollBar::add-line:vertical {\n"
+"     border: none;\n"
+"    background: rgb(55, 63, 77);\n"
+"     height: 20px;\n"
+"	border-bottom-left-radius: 4px;\n"
+"    border-bottom-right-radius: 4px;\n"
+"     subcontrol-position: bottom;\n"
+"     subcontrol-origin: margin;\n"
+" }\n"
+" QScrollBar::sub-line:vertical {\n"
+"	border: none;\n"
+"    background: rgb(55, 63, 77);\n"
+"     height: 20px;\n"
+"	border-top-left-radius: 4px;\n"
+"    border-top-right-radius: 4px;\n"
+"     subcontrol-position: top;\n"
+"     subcontrol-origin: margin;\n"
+" }\n"
+" QScrollBar::up-arrow:vertical, QScrollBar:"
+                        ":down-arrow:vertical {\n"
+"     background: none;\n"
+" }\n"
+"\n"
+" QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {\n"
+"     background: none;\n"
+" }")
+
+        self.horizontalLayout_2.addWidget(self.textEdit)
+
+
+        self.verticalLayout_2.addWidget(self.frame)
+
+        self.bottomframe = QFrame(self.bg)
+        self.bottomframe.setObjectName(u"bottomframe")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Ignored)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.bottomframe.sizePolicy().hasHeightForWidth())
+        self.bottomframe.setSizePolicy(sizePolicy)
+        self.bottomframe.setMaximumSize(QSize(16777215, 25))
+        self.bottomframe.setSizeIncrement(QSize(0, 25))
+        self.bottomframe.setFrameShape(QFrame.StyledPanel)
+        self.bottomframe.setFrameShadow(QFrame.Raised)
+
+        self.verticalLayout_2.addWidget(self.bottomframe)
+
+
+        self.verticalLayout.addWidget(self.bg)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Python Code Editor", None))
+        self.titleBar.setText(QCoreApplication.translate("MainWindow", u"Python Code Editor", None))
+#if QT_CONFIG(tooltip)
+        self.runFileBtn.setToolTip(QCoreApplication.translate("MainWindow", u"Run File in Terminal (F5)", None))
+#endif // QT_CONFIG(tooltip)
+        self.runFileBtn.setText("")
+#if QT_CONFIG(shortcut)
+        self.runFileBtn.setShortcut(QCoreApplication.translate("MainWindow", u"F5", None))
+#endif // QT_CONFIG(shortcut)
+#if QT_CONFIG(tooltip)
+        self.openFileBtn.setToolTip(QCoreApplication.translate("MainWindow", u"Open File (ctrl + o)", None))
+#endif // QT_CONFIG(tooltip)
+        self.openFileBtn.setText("")
+#if QT_CONFIG(shortcut)
+        self.openFileBtn.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+O", None))
+#endif // QT_CONFIG(shortcut)
+#if QT_CONFIG(tooltip)
+        self.saveAsBtn.setToolTip(QCoreApplication.translate("MainWindow", u"Save As (ctrl + Shit + s)", None))
+#endif // QT_CONFIG(tooltip)
+        self.saveAsBtn.setText("")
+#if QT_CONFIG(shortcut)
+        self.saveAsBtn.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+Shift+S", None))
+#endif // QT_CONFIG(shortcut)
+#if QT_CONFIG(tooltip)
+        self.saveFileBtn.setToolTip(QCoreApplication.translate("MainWindow", u"Save File (ctrl + s)", None))
+#endif // QT_CONFIG(tooltip)
+        self.saveFileBtn.setText("")
+#if QT_CONFIG(shortcut)
+        self.saveFileBtn.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
+#endif // QT_CONFIG(shortcut)
+#if QT_CONFIG(tooltip)
+        self.minimizeBtn.setToolTip(QCoreApplication.translate("MainWindow", u"Minimize", None))
+#endif // QT_CONFIG(tooltip)
+        self.minimizeBtn.setText("")
+#if QT_CONFIG(tooltip)
+        self.maxBtn.setToolTip(QCoreApplication.translate("MainWindow", u"Maximize", None))
+#endif // QT_CONFIG(tooltip)
+        self.maxBtn.setText("")
+#if QT_CONFIG(tooltip)
+        self.closeBtn.setToolTip(QCoreApplication.translate("MainWindow", u"Close", None))
+#endif // QT_CONFIG(tooltip)
+        self.closeBtn.setText("")
+    # retranslateUi
+
